9bf83b916ab5488d376678b421ded4f833bb7130
johnny <johnaroza@ymail.com>
2016-11-30 02:04:01 +0530

    a
    a

--- Voting_System/dbh.php
+++
@@ -1,7 +0,0 @@
-<?php
-	//mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-   $conn = mysqli_connect("localhost","root","bs221","voting_login");
-   if (!$conn) {
-   	die("connection failed".mysqli_connect_error());
-   }
-?>
